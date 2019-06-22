9489c1af7af8cfd90303e06b864f8c8b3304ef8d
sasihari <32600713+sasihari@users.noreply.github.com>
2019-06-22 11:58:39 +0530

    new change
    while loop is added

--- speech _recogni.py
+++ speech _recogni.py
@@ -1,11 +1,14 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
+while True :
+ r= sr.Recognizer()
+
+ with sr.Microphone() as source:
     print("Speak Anything :")
     audio = r.listen(source)
+
     try:
         text = r.recognize_google(audio)
         print("You said : {}".format(text))
-    except:
+    except sr.UnknownValueError:
         print("Sorry could not recognize what you said")
